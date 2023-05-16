42e3e5d8396112b79c94a9c28b11e14b5e78c894
Raj Patel <rajmpatel97@gmail.com>
2023-05-16 14:04:25 -0400

    Login service Init

---
+++ src/main/java/com/miit_api/processor_api/exception/ResourceNotFoundException.java
@@ -0,0 +1,6 @@
+package com.miit_api.processor_api.exception;
+
+public class ResourceNotFoundException extends Throwable {
+    public ResourceNotFoundException(String account_not_found) {
+    }
+}
